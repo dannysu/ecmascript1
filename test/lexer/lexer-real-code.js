b636ee3d3fba2d2541b2e4e3e0911f0fa29afdd2
Danny Su <contact@dannysu.com>
2016-01-04 02:00:16 -0800

    Test using jquery source code as well

--- test/lexer/lexer-real-code.js
+++ test/lexer/lexer-real-code.js
@@ -4,11 +4,18 @@ const should = require('should');
 
 const lexer = require('../../src/lexer.js');
 const codemirror = require('../codemirror-string.js');
+const jquery = require('../jquery-string.js');
 
 describe('lexer', function() {
     describe('parse CodeMirror code', function() {
         it('should succeed', function() {
             lexer.setInput(codemirror.codemirror30);
+        });
+    });
+
+    describe('parse jQuery code', function() {
+        it('should succeed', function() {
+            lexer.setInput(jquery.jquery111);
         });
     });
 });
