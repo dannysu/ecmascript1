c5b6fa5ea26a108d6746eeb48924b1359290c46b
Danny Su <contact@dannysu.com>
2016-01-07 21:34:56 -0800

    Make sure to test nested if-statements too

--- test/parser/parser-if-statements.js
+++ test/parser/parser-if-statements.js
@@ -96,4 +96,40 @@ describe('parser', function() {
             ast.body[0].alternate.expression.value.should.be.eql('3');
         });
     });
+
+    describe('parse a nested if-statement', function() {
+        it('should return proper ESTree AST', function() {
+            const input = `
+                if (1)
+                    if (2)
+                        3;
+                    else
+                        4;
+                else 5;
+            `;
+            const ast = parser.parse(input);
+
+            ast.type.should.be.eql('Program');
+            should.exist(ast.body);
+            ast.body.length.should.be.eql(1);
+
+            ast.body[0].type.should.be.eql('IfStatement');
+            ast.body[0].test.type.should.be.eql('Literal');
+            ast.body[0].test.value.should.be.eql('1');
+            ast.body[0].alternate.type.should.be.eql('ExpressionStatement');
+            ast.body[0].alternate.expression.type.should.be.eql('Literal');
+            ast.body[0].alternate.expression.value.should.be.eql('5');
+
+            const innerIf = ast.body[0].consequence;
+            innerIf.type.should.be.eql('IfStatement');
+            innerIf.test.type.should.be.eql('Literal');
+            innerIf.test.value.should.be.eql('2');
+            innerIf.consequence.type.should.be.eql('ExpressionStatement');
+            innerIf.consequence.expression.type.should.be.eql('Literal');
+            innerIf.consequence.expression.value.should.be.eql('3');
+            innerIf.alternate.type.should.be.eql('ExpressionStatement');
+            innerIf.alternate.expression.type.should.be.eql('Literal');
+            innerIf.alternate.expression.value.should.be.eql('4');
+        });
+    });
 });
